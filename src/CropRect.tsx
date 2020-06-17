30c03a115705f90cd8da7934384291b94e109b6d
puresick <2714266+puresick@users.noreply.github.com>
2020-06-17 13:47:04 +0200

    Add first CropRect component with basic functionality

---
+++ src/CropRect.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react"
+import Draggable from "react-draggable"
+
+import "./CropRect.css"
+import CropRectResizeHandler from "./CropRectResizeHandler"
+
+interface CropRect {
+	x: number
+	y: number
+	width: number
+	height: number
+}
+
+function handleDrag(e: MouseEvent, data: Object) {
+	console.log("dragging")
+	console.log(e)
+	console.log(data)
+}
+
+function CropRect() {
+	const [cropRect, setCropRect] = useState<CropRect>({
+		x: 0,
+		y: 0,
+		width: 100,
+		height: 100
+	})
+
+	return (
+		<Draggable
+			bounds="main"
+			onStop={(e, data) => {}}
+			cancel=".croparea--hotcorner"
+		>
+			<section
+				className="croparea"
+				style={{
+					width: cropRect.width,
+					height: cropRect.height,
+					top: cropRect.y,
+					left: cropRect.x
+				}}
+			>
+				<CropRectResizeHandler cropRect={cropRect} setCropRect={setCropRect} handlePosition="topleft"/>
+				<CropRectResizeHandler cropRect={cropRect} setCropRect={setCropRect} handlePosition="topright"/>
+				<CropRectResizeHandler cropRect={cropRect} setCropRect={setCropRect} handlePosition="bottomleft"/>
+				<CropRectResizeHandler cropRect={cropRect} setCropRect={setCropRect} handlePosition="bottomright"/>
+			</section>
+		</Draggable>
+	)
+}
+
+export default CropRect
---
+++ src/CropRectResizeHandler.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react"
+import Draggable from "react-draggable"
+
+import "./CropRectResizeHandler.css"
+
+interface CropRectResizeHandlerProps {
+	cropRect: Object
+	handlePosition: String
+	setCropRect: Function
+}
+
+interface CropRect {
+	x: number
+	y: number
+	width: number
+	height: number
+}
+
+interface DraggableData {
+	node: HTMLElement
+	x: number
+	y: number
+	deltaX: number
+	deltaY: number
+	lastX: number
+	lastY: number
+}
+
+function updateCropRect(
+	oldCrop: CropRect,
+	data: DraggableData,
+	handlePosition: String
+) {
+	const { height: oldHeight, width: oldWidth, x: oldX, y: oldY } = oldCrop
+	const { deltaX, deltaY } = data
+
+	const copyCrop = {
+		...oldCrop,
+		width:
+			handlePosition === "topleft" || handlePosition === "bottomleft"
+				? oldWidth - deltaX
+				: oldWidth + deltaX,
+		height:
+			handlePosition === "topleft" || handlePosition === "topright"
+				? oldHeight - deltaY
+				: oldHeight + deltaY,
+		x:
+			handlePosition === "topleft" || handlePosition === "bottomleft"
+				? oldX + deltaX
+				: oldX,
+		y:
+			handlePosition === "topleft" || handlePosition === "topright"
+				? oldY + deltaY
+				: oldY
+	}
+
+	return copyCrop
+}
+
+function CropRectResizeHandler(props: CropRectResizeHandlerProps) {
+	const { handlePosition, setCropRect } = props
+
+	return (
+		<Draggable
+			onDrag={(e, data) => {
+				setCropRect((oldCrop: CropRect) =>
+					updateCropRect(oldCrop, data, handlePosition)
+				)
+			}}
+			position={{ x: 0, y: 0 }}
+		>
+			<div className={`croparea--hotcorner position-${handlePosition}`}></div>
+		</Draggable>
+	)
+}
+
+export default CropRectResizeHandler
